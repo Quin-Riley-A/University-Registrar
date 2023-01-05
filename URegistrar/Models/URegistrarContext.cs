2f138e37a2a7af9aa8d5efb63d8b3a80ec897ccc
gramsower <110431307+gramsower@users.noreply.github.com>
2023-01-04 19:32:35 -0800

    initial commit
    Co-authored-by Quin Asselin <quin.asselin1@gmail.com>

---
+++ URegistrar/Models/URegistrarContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace URegistrar.Models
+{
+  public class URegistrarContext : DbContext
+  {
+    public DbSet<Student> Students { get; set; }
+    //public DbSet<Course> Courses { get; set; }
+    public URegistrarContext (DbContextOptions options) : base (options) { }
+  }
+}
